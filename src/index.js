34ea1ae595da56624062786161eb46dcbb7a231f
Nitin Khandelwal <nkhandelwal@XEBIAINDIA.LOCAL>
2017-11-20 11:57:23 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import _ from 'lodash';
+
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyANjYtg24njoeEh64RLeVv6_Fhzp_af00k';
+//Create a component that will produce HTML
+
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null,
+            term: 'surfboard'
+        };
+        this.videoSearch('surfboards');
+
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
+            console.log(videos);
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });     // {videos: videos} is equivalent to {videos}
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange = {videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => { this.setState({ selectedVideo }) }}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+
+
+
+//Insert into DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
